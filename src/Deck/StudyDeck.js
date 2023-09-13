8bb7f2e780a9ca28080654f44ab93cc8697cea81
Ethan Wiegert <ewiegert99@gmail.com>
2023-09-13 14:24:04 -0400

    Initial work on StudyDeck.

--- src/Deck/StudyDeck.js
+++ src/Deck/StudyDeck.js
@@ -4,6 +4,38 @@ import {readDeck, deleteDeck, deleteCard} from "../utils/api"
 
 
 function StudyDeck(){
+
+    const {deckId}=useParams()
+
+    const [deck, setDeck]=useState([])
+
+
+    useEffect(() => {
+        async function loadDeck() {
+          const abortController = new AbortController();
+          try {
+            const response = await readDeck(deckId, abortController.signal);
+            setDeck(response);
+          } catch (e) {
+            console.log(e.name);
+          }
+          return () => {
+            abortController.abort();
+          };
+        }
+        loadDeck();
+      }, []);
+
+
+      return (
+        <div>
+          <h5>Home/{deck.name}/Study</h5>
+          <p></p>
+          <buton></buton>
+        </div>
+      )
+     
 }
 
 export default StudyDeck
+
