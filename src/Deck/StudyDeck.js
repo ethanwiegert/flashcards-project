56a815a1e5ef0a6e88eaedd3e4d84d45cd3e2bd7
Ethan Wiegert <ewiegert99@gmail.com>
2023-09-15 14:55:07 -0400

    Completing style elements and links in StudyDeck.

--- src/Deck/StudyDeck.js
+++ src/Deck/StudyDeck.js
@@ -98,7 +98,7 @@ else if (!isItFlipped){
          <nav aria-label="breadcrumb">
   <ol class="breadcrumb">
     <li class="breadcrumb-item"><a href="/">Home</a></li>
-    <li class="breadcrumb-item"><a href="{`/decks/${deckId}`}">{deck.name}</a></li>
+    <li class="breadcrumb-item"><a href={`/decks/${deckId}`}>{deck.name}</a></li>
     <li class="breadcrumb-item active" aria-current="page">Study</li>
   </ol>
 </nav>
