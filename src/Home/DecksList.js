fdcd19228c693b63afcc72acac04b9987832c54d
Ethan Wiegert <ewiegert99@gmail.com>
2023-09-18 14:04:15 -0400

    Updating delete, displaying card number

--- src/Home/DecksList.js
+++ src/Home/DecksList.js
@@ -1,6 +1,6 @@
 import {useEffect, useState} from "react"
 import {Link, useHistory} from "react-router-dom"
-import {deleteDeck, listDecks} from "../utils/api"
+import {deleteDeck, listDecks, readDeck} from "../utils/api"
 
 
 function DecksList(){
@@ -9,6 +9,7 @@ function DecksList(){
 
 
 const [decks, setDecks] = useState([]);
+const [cards, setCards] = useState([]);
 
 useEffect(() => {
   async function loadDecks() {
@@ -26,12 +27,25 @@ useEffect(() => {
   loadDecks();
 }, []);
 
+useEffect(() => {
+  const abortController = new AbortController();
 
-const handleDeckDelete = async (event) => {
+  async function displayDeck(deckId) {
+      const response = await readDeck(deckId, abortController.signal);
+      setCards(response);
+  }
+      
+      displayDeck();
+          return () => abortController.abort();
+        },[deckId]);
+      
+
+
+const handleDeckDelete = async (deckId) => {
   if (
     window.confirm("Are you sure you want to delete this deck?")
   ) {
-    await deleteDeck(deckId, event);
+    await deleteDeck(deckId);
     history.go(0);
   }
 };
@@ -58,7 +72,7 @@ const handleCreate = (event) =>{
   <div className="card-body">
     <div className="row card-text">
     <h5 className="col-10">{deck.name}</h5>
-    <p className="col-2">{decks.length} Cards</p>
+    <p className="col-2">{cards.length} Cards</p>
     </div>
     <div className="row d-flex">
     <p className="card-text col-12">{deck.description}</p>
@@ -66,7 +80,7 @@ const handleCreate = (event) =>{
     <div className="row pt-3 d-flex flex-row">
   <div className="col-1"><Link to={`/decks/${deck.id}`}><button type="button" className="btn btn-secondary flex-row">View</button></Link></div>
   <div className="col-9"><Link to={`/decks/${deck.id}/study`}><button type="button" className="btn btn-primary d-flex flex-row">Study</button></Link></div>
-  <div className="col-2"><button type="button" className="btn btn-danger d-flex justify-content-end" onClick={handleDeckDelete}>Delete</button>
+  <div className="col-2"><button type="button" className="btn btn-danger d-flex justify-content-end" onClick={()=>handleDeckDelete(deck.id)}>Delete</button>
     </div>
 </div>
   </div>
