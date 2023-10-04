a163628cd89d1f3e57044bb44bd34a01c5736e21
Ethan Wiegert <ewiegert99@gmail.com>
2023-10-04 17:41:20 -0400

    Refactoring DeckList and adding DeleteDeck.

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,16 +1,22 @@
+import React from "react";
+import DeckList from "./DeckList";
+import { useHistory} from "react-router-dom";
 
-import DecksList from "./DecksList";
+function Home() {
+const history = useHistory();
+return (
+<div>
+<button
+className="btn btn-secondary"
+type="button"
+onClick={() => history.push("/decks/new")}>
+<span className="oi oi-plus mr-1"></span>
+Create Deck
+</button>
 
-
-function Home(){
-
-
-  return(
-    <DecksList />
-  )
-
+<DeckList/>
+</div>
+)
 }
-
-  
-  export default Home;
+export default Home;
   
